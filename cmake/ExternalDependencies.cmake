efb635fa00ab1e0c0997cf7a2b53e501dd157b97
yanjun <541726449@qq.com>
2020-08-15 23:01:58 +0800

    add git_tag

--- cmake/ExternalDependencies.cmake
+++ cmake/ExternalDependencies.cmake
@@ -82,7 +82,7 @@ find_package(GLog REQUIRED)
 ExternalProject_Add(
     gtest
     GIT_REPOSITORY https://github.com/google/googletest.git
-    # GIT_TAG v1.10.x
+    GIT_TAG v1.10.x
     TIMEOUT 10
     PREFIX ${CMAKE_CURRENT_BINARY_DIR}/third_party/gtest
     # no install required, we link the library from the build tree
