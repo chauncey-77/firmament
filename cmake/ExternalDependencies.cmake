b95482a0632d4ba7a877938119ac8702f1d8bbe9
xuchangtiao <xuchangtiao_77@outlook.com>
2020-08-18 19:19:07 +0800

    change gtest's branch

--- cmake/ExternalDependencies.cmake
+++ cmake/ExternalDependencies.cmake
@@ -82,7 +82,7 @@ find_package(GLog REQUIRED)
 ExternalProject_Add(
     gtest
     GIT_REPOSITORY https://github.com/google/googletest.git
-    GIT_TAG v1.10.x
+    GIT_TAG "master"
 	GIT_SHALLOW ON
     TIMEOUT 10
     PREFIX ${CMAKE_CURRENT_BINARY_DIR}/third_party/gtest
